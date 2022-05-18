fcb33a5c45ff9d4e8ede428dc994f94bc9af17b3
Hamed <high_dev@outlook.com>
2022-05-18 15:01:03 +0430

    Added font awesome and my custom font

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 const App = ()=> {
   return (
     <div className="App">
-        <h3>مدیریت مخاطبین</h3>
+        <h3 >مدیریت مخاطبین</h3>
+        <span className="fa fa-user"></span>
+        <span className="fab fa-app-store-ios"></span>
         <button className="btn btn-primary">Button</button>
     </div>
   );
