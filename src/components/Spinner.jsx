26d936301a655be8e4ce5436c3d81ea98a5f5820
Hamed <high_dev@outlook.com>
2022-05-22 12:51:54 +0430

    Add Spinner component

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,12 @@
+import spinnerGIF from '../assets/Spinner.gif'
+
+const Spinner = () => {
+    return(
+        <>
+            <img src={spinnerGIF} alt="" className="d-block m-auto" style={{width: '200px'}}/>
+        </>
+    )
+}
+
+
+export default Spinner
