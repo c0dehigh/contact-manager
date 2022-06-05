07b96a99d04ecb19b78622222b900c033bb4d57a
Hamed <high_dev@outlook.com>
2022-06-05 10:32:54 +0430

    add axios and jsx

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import {useState} from "react";
-import {AddContact,EditContact,ViewContact,Contacts,Contact,Navbar} from './components'
-import {Routes,Route, Navigate} from "react-router-dom"
-import './App.css';
+import { useState, useEffect } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import axios from "axios";
+import {
+  AddContact,
+  EditContact,
+  ViewContact,
+  Contacts,
+  Contact,
+  Navbar,
+} from "./components";
 
-const App = ()=> {
-    const [loading,setLoading] = useState(false)
-    const [getcontacts,setContacts] = useState([])
+import "./App.css";
+
+const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [getcontacts, setContacts] = useState([]);
+  const [getGroups, setGroups] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const { data: contactsData } = await axios.get(
+          "http://localhost:9000/contacts"
+        );
+        const { data: groupsData } = await axios.get(
+          "http://localhost:9000/groups"
+        );
+        setContacts(contactsData);
+        setGroups(groupsData);
+        setLoading(false);
+      } catch (err) {
+        console.log(err.message);
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-       <Navbar/>
-        <Routes>
-            <Route path="/" element={<Navigate to="/contacts"/>} />
-            <Route path="/contacts" element={<Contacts contacts={getcontacts} loading={loading}/>} />
-            <Route path="/contacts/add" element={<AddContact/>}/>
-            <Route path="/contacts/:contactId" element={<ViewContact/>} />
-            <Route path="/contacts/edit/:contactId" element={<EditContact/>}  />
-        </Routes>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Navigate to="/contacts" />} />
+        <Route
+          path="/contacts"
+          element={<Contacts contacts={getcontacts} loading={loading} />}
+        />
+        <Route path="/contacts/add" element={<AddContact />} />
+        <Route path="/contacts/:contactId" element={<ViewContact />} />
+        <Route path="/contacts/edit/:contactId" element={<EditContact />} />
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
